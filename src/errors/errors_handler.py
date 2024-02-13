e98ded5afa459d6b49568f6c0d2d89fb467c4753
ganimedes <ganimedes409@gmail.com>
2024-02-13 14:34:15 -0300

    feat: Implementing validator

---
+++ src/errors/errors_handler.py
@@ -0,0 +1,21 @@
+from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity_error import HttpUnprocessableEntityError
+
+
+def handler_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+              "errors": [{
+                "title": error.name, 
+                "detail": error.message
+              }]
+            })
+
+
+    return HttpResponse(status_code=500, body={
+      "errors": [{
+          "title": "Server Error", 
+          "detail": str(error)}
+      ]})
